55a81d7978409545288f48ff040454b335cadef3
David Rosskopf <drosskopf@ldschurch.org>
2017-08-30 14:48:23 -0600

    missing files for vcap

---
+++ Extensions/JsonStreamConfigurationProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace webapp
+{
+    public class JsonStreamConfigurationProvider : JsonConfigurationProvider
+    {
+        //// SourceRef: https://github.com/SteeltoeOSS/Configuration
+
+        MemoryStream _stream;
+        internal JsonStreamConfigurationProvider(JsonConfigurationSource source, MemoryStream stream) : base(source)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+            _stream = stream;
+        }
+        public override void Load()
+        {
+            base.Load(_stream);
+        }
+    }
+}
---
+++ Extensions/JsonStreamConfigurationSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace webapp
+{
+    //// SourceRef: https://github.com/SteeltoeOSS/Configuration
+
+    public class JsonStreamConfigurationSource : JsonConfigurationSource
+    {
+        private MemoryStream _stream;
+
+        internal JsonStreamConfigurationSource(MemoryStream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+            _stream = stream;
+        }
+        public override IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new JsonStreamConfigurationProvider(this, _stream);
+        }
+    }
+}
